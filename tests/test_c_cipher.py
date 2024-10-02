b5fb2c8beb2af74c9440b7c3e13423ac174e9414
lovvgh <btw18@sunyorange.edu>
2024-10-02 12:31:46 -0400

    v1.2_test_c_cipher.py

--- tests/test_c_cipher.py
+++ tests/test_c_cipher.py
@@ -29,9 +29,10 @@ def test_successful_encryption(caplog):
     assert result == "def345"
     assert "def345" not in caplog.text 
 
-'''
+"""
 def test_kick_the_back_tire():
     assert decrypt() == 'aef345'
+"""
 
 def test_decrypt_length_error(caplog):
     """Test that a length error message is logged for invalid email size"""
@@ -54,4 +55,3 @@ def test_successful_decryption(caplog):
     result = decrypt("def345")
     assert result == "abc012"
     assert "abc012" not in caplog.text 
-'''
